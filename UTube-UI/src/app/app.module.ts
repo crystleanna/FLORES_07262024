811215ccc1eeaec2e52c61da43ac6c2f179d4ed3
Crystle Ann Flores <crystleannflores18@gmail.com>
2024-07-26 13:33:24 +0800

    Create the routes
    Created the Home, Upload, and Stream routes

--- UTube-UI/src/app/app.module.ts
+++ UTube-UI/src/app/app.module.ts
@@ -4,14 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
+import { StreamComponent } from './stream/stream.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'upload', component: UploadComponent},
+  {path: 'stream', component: StreamComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UploadComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}  // <-- debugging purposes only
+      ),
     NgbModule
   ],
   providers: [],
