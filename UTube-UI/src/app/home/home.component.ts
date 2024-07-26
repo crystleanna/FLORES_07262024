8d37b021c17be572f44547f2b595051624f79f22
Crystle Ann Flores <crystleannflores18@gmail.com>
2024-07-26 14:13:46 +0800

    Initial Home layout

--- UTube-UI/src/app/home/home.component.ts
+++ UTube-UI/src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Video } from '../models/video.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  videos: Video[] = []
 
+  ngAfterViewInit(): void {
+    var sampleVideo = new Video()
+    sampleVideo.title = "Super Extra Long Title That Will Break The Layout"
+    sampleVideo.description = "description"
+    sampleVideo.thumbnail = "thumbnail"
+    sampleVideo.url = "url"
+    
+    this.videos = [sampleVideo, sampleVideo, sampleVideo, sampleVideo]
+  }
 }
+
